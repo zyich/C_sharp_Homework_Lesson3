1282d9e6a934361a4f394faf104fad3a159f1152
zyich <zuilinho@yandex.ru>
2023-06-22 21:11:58 +0200

    task3 find all cubenumber between 1 and Random number

---
+++ task3(cube_number_from1_toN)/Program.cs
@@ -0,0 +1,30 @@
+﻿/* ЯЗадача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int RandomNumber(int min, int max)
+{
+    int result = new Random().Next(min,max+1);
+
+    return result;
+
+}
+
+void Print(string text)
+{
+    Console.WriteLine(text);
+}
+
+void GetAllSquare(int rand )
+{
+    for (int i=1;i<=rand;i++)
+    {
+        Console.Write($"{Math.Pow(i,3)}, ");
+    }
+}
+
+int rand = RandomNumber(1,10);// между мин и макс
+Print($"{rand}");
+GetAllSquare(rand);
